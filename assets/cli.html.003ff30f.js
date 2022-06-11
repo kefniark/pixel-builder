bc68a7588ac1b59d83bf68828d5c751d8af9fe8f
kefniark <kefniark@users.noreply.github.com>
2022-06-11 07:14:13 +0000

    Deploying to gh-pages from @ kefniark/pixel-builder@bf71cfeec7eb2278e056329d01ff9c3242f2e58f 🚀

---
+++ assets/cli.html.003ff30f.js
@@ -0,0 +1,20 @@
+import{_ as n,o as a,c as s,a as e}from"./app.f029edd0.js";const i={},l=e(`<h1 id="pixel-cli" tabindex="-1"><a class="header-anchor" href="#pixel-cli" aria-hidden="true">#</a> Pixel CLI</h1><h2 id="description" tabindex="-1"><a class="header-anchor" href="#description" aria-hidden="true">#</a> Description</h2><p>The Pixel CLI is the core of pixel builder, it&#39;s what allow to create, develop and build projects.</p><h2 id="to-install-or-update" tabindex="-1"><a class="header-anchor" href="#to-install-or-update" aria-hidden="true">#</a> To install or update</h2><div class="language-bash ext-sh line-numbers-mode"><pre class="language-bash"><code><span class="token function">yarn</span> global <span class="token function">add</span> @pixel-builder/cli
+
+<span class="token comment"># or npn users</span>
+$ <span class="token function">npm</span> i -g @pixel-builder/cli
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><hr><h2 id="usage" tabindex="-1"><a class="header-anchor" href="#usage" aria-hidden="true">#</a> Usage</h2><div class="language-bash ext-sh line-numbers-mode"><pre class="language-bash"><code><span class="token comment"># &gt;&gt; pixel help</span>
+Usage: Pixel Builder <span class="token punctuation">[</span>options<span class="token punctuation">]</span> <span class="token punctuation">[</span>command<span class="token punctuation">]</span>
+
+CLI to create and build HTML5 Games
+
+Options:
+  -h, --help                 display <span class="token builtin class-name">help</span> <span class="token keyword">for</span> <span class="token builtin class-name">command</span>
+
+Commands:
+  create <span class="token punctuation">[</span>options<span class="token punctuation">]</span> <span class="token punctuation">[</span>string<span class="token punctuation">]</span>  Create a new pixel project
+  pack <span class="token punctuation">[</span>options<span class="token punctuation">]</span> <span class="token punctuation">[</span>string<span class="token punctuation">]</span>    Auto-Generate assets <span class="token keyword">for</span> a Pixel Project <span class="token punctuation">(</span>spritesheet<span class="token punctuation">)</span>
+  dev                        Run a Pixel Project
+  build                      Build a Project Project
+  preview                    Preview a Build of a Pixel Project
+  <span class="token builtin class-name">help</span> <span class="token punctuation">[</span>command<span class="token punctuation">]</span>             display <span class="token builtin class-name">help</span> <span class="token keyword">for</span> <span class="token builtin class-name">command</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,8),t=[l];function c(o,p){return a(),s("div",null,t)}var r=n(i,[["render",c],["__file","cli.html.vue"]]);export{r as default};
---
+++ assets/getting-started.html.4abd1873.js
@@ -0,0 +1,14 @@
+import{_ as e,o as n,c as a,a as s}from"./app.f029edd0.js";const i={},t=s(`<h1 id="getting-started" tabindex="-1"><a class="header-anchor" href="#getting-started" aria-hidden="true">#</a> Getting Started</h1><h2 id="install-our-cli" tabindex="-1"><a class="header-anchor" href="#install-our-cli" aria-hidden="true">#</a> Install our CLI</h2><p>To get started, run the following to get our CLI tools:</p><div class="language-bash ext-sh line-numbers-mode"><pre class="language-bash"><code><span class="token function">yarn</span> global <span class="token function">add</span> @pixel-builder/cli
+
+<span class="token comment"># or npn users</span>
+$ <span class="token function">npm</span> i -g @pixel-builder/cli
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="create-a-new-pixel-project" tabindex="-1"><a class="header-anchor" href="#create-a-new-pixel-project" aria-hidden="true">#</a> Create a New Pixel Project</h2><p>From there, you can create a new project</p><div class="language-bash ext-sh line-numbers-mode"><pre class="language-bash"><code>pixel create ProjectName
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><p>You will be asked few questions about what kind of project and options you would like.</p><p>Then when it&#39;s finished:</p><div class="language-bash ext-sh line-numbers-mode"><pre class="language-bash"><code><span class="token comment"># open your project</span>
+<span class="token builtin class-name">cd</span> ProjectName
+
+<span class="token comment"># run in develop</span>
+<span class="token function">yarn</span> dev
+
+<span class="token comment"># or build it</span>
+<span class="token function">yarn</span> build
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>For more informations, check the <strong>./README.md</strong> at the root of your new project</p>`,11),r=[t];function l(d,c){return n(),a("div",null,r)}var u=e(i,[["render",l],["__file","getting-started.html.vue"]]);export{u as default};
